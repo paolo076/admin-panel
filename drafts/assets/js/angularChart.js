a719895ff51cf78736355e88df61776463ca31c1
Unknown <pao_soriano@yahoo.com.ph>
2017-07-12 16:39:15 +0800

    Updated pie charts and added user profile
    - charts needs tweaking

--- drafts/assets/js/angularChart.js
+++ drafts/assets/js/angularChart.js
@@ -1,6 +1,11 @@
 var app = angular.module("myApp",["chart.js"]);
 
 app.controller("PieCtrl", function($scope){
-    $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Upload Sales"];
-    $scope.data = [10,10,20,30];        
+    $scope.labels = ["Download Sales", "In-Store Sales", "Upload Sales"];
+    $scope.data = [30,40,30];   
+    $scope.colors = ["#5DA5DA","#FAA43A","#F15854"];
+    $scope.options = {
+        responsive: false,
+        maintainAspectRatio: false
+    }    
 });
